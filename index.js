cdfea8d81d4381a6de48cae25c7cb0310289eb4a
guilherme.costa <rrJ@jZfuiQKM&z>
2022-06-24 08:41:04 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const { response } = require("express");
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  return res.json({ message: "Server is up" });
+});
+
+app.post("/teste", (req, res) => {
+  const { name, date } = req.body;
+
+  return res.json({ name, date });
+});
+
+app.listen(3333);
