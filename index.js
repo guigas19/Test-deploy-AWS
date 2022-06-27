0f6dfedc4659f55932e3500fc34c13447f3aeedb
guilherme.costa <rrJ@jZfuiQKM&z>
2022-06-26 22:24:55 -0300

    add script build

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  return res.json({ message: "Server is up" });
+  return res.json({ message: "Server is up!" });
 });
 
 app.get("/newrouter", (req, res) => {
