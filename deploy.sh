64ef1fd7d31edf9832e29c7103fb1782f40afefa
guilherme.costa <rrJ@jZfuiQKM&z>
2022-06-26 21:13:31 -0300

    add script build

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@ echo "Kill all the running PM2 actions"
 sudo pm2 kill
 
 echo "Jump to app folder"
-cd /home/ubuntu/app-name
+cd /home/ubuntu/Test-deploy-AWS
 
 echo "Update app from Git"
 git pull
@@ -15,5 +15,5 @@ echo "Build your app"
 sudo npm run build
 
 echo "Run new PM2 action"
-sudo cp /home/ubuntu/ecosystem.json ecosystem.json
+sudo cp /home/ubuntu/Test-deploy-AWS/ecosystem.json ecosystem.json
 sudo pm2 start ecosystem.json
